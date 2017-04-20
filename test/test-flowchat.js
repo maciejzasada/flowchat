2026c65396f6964a58d61a52399f917d932eaada
Maciej Zasada <maciej@unit9.com>
2017-04-20 15:07:01 +0200

    basic structure scaffold

---
+++ test/test-flowchat.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+
+import Test from 'tape';
+import Flowchat from '../dist/lib';
+
+Test('test flowchat', (t) => {
+
+    t.test('plan', (t) => {
+        t.plan(1);
+
+        t.ok(true);
+    });
+
+});
