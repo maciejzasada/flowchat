e564cf19c39f333d1ff9f7ba7ff0843b0458878d
Maciej Zasada <maciej@unit9.com>
2017-06-29 07:54:16 +0200

    working FB echo bot

---
+++ examples/facebook-messenger/lib/index_template.js
@@ -0,0 +1,39 @@
+import { Flowchat } from 'flowchat';
+import readline from 'readline';
+
+import { helloFlow } from './flows/helloFlow';
+
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+const bot = new Flowchat();
+const userId = Math.round(Math.random() * 999);
+const states = {};
+
+bot.flow('/hello', ...helloFlow);
+
+bot.state.subscribe(({ state, sessionId, wait }) => {
+  const promise = new Promise(resolve => {
+    states[sessionId] = state;
+    setTimeout(resolve, 1000);
+  });
+  wait(promise);
+});
+
+bot.output
+.subscribe(({ data, sessionId }) => {
+  console.log(`Bot -> ${sessionId}: ${data}`);
+  rl.prompt();
+});
+
+function main() {
+  console.log('\n*** Press CRTL+C to end the chat ***\n');
+  rl.setPrompt(`${userId}: `);
+  rl.prompt();
+  rl.on('line', function(line) {
+    if (!states[userId]) {
+      states[userId] = { saidHello: false };
+    }
+    bot.input.onNext({ data: line, state: states[userId], sessionId: userId });
+  });
+}
+
+main();
